96d55668b8f27e351802f6f3ec9a0811ec346fad
Andrew <andrewphillipheil@facebook.com>
2013-02-14 18:51:10 -0500

    Created rails app and added trips

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tourist::Application.config.secret_token = 'f46cfdca5e94de9c833ea20dfa7cd8a790698cf6fdf2ea2a8bee148b6ee085bf3fd61f67a60a1a13a7a7e6347446a691754052333c69bc7adbf17af9d8b2e05e'

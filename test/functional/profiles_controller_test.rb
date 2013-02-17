20938ce48ce1683d0e86c5b3f82d90186d03f6c0
Andrew <andrewphillipheil@facebook.com>
2013-02-17 02:15:49 -0500

    Updated profile page and added scoped status updates to it

--- test/functional/profiles_controller_test.rb
+++ test/functional/profiles_controller_test.rb
@@ -2,8 +2,27 @@ require 'test_helper'
 
 class ProfilesControllerTest < ActionController::TestCase
   test "should get show" do
-    get :show
+    get :show, id: users(:andrew).profile_name
     assert_response :success
+    assert_template 'profiles/show'
+  end
+
+  test "should render a 404 on profile not found" do
+  	get :show, id: "doesn't exist"
+  	assert_response :not_found
+  end
+
+  test "that variables are assigned on successful profiles" do
+  	get :show, id: users(:andrew).profile_name
+  	assert assigns(:user)
+  	assert_not_empty assigns(:trips)
+  end
+
+  test "only show the correct statuses are shown" do
+    get :show, id: users(:andrew).profile_name
+    assigns(:trips).each do |trip|
+      assert_equal users(:andrew), trip.user
+    end
   end
 
 end
